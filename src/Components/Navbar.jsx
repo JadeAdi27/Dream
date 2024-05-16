43d7c23e04ee7ba66d1328a282e62d995939116f
Manvaditya <eurekajade27@gmail.com>
2024-05-16 20:51:43 +0530

    vishesh special changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-black h-[100px]">
+    <nav className="bg-customBlue h-[75px]">
       <div className="flex justify-end items-center h-full">
         <div className="text-white">
-          <Link to="/" className="text-gray-300 text-2xl hover:text-white">Home</Link>
-          <Link to="/login" className="text-gray-300 text-2xl ml-10 mr-9 hover:text-white">Login</Link>
+          <Link to="/" className="text-white text-2xl hover:text-blue-500">Home</Link>
+          <Link to="/login" className="text-white text-2xl ml-10 mr-9 hover:text-blue-500">Login</Link>
         </div>
       </div>
     </nav>
--- src/Components/Sign.jsx
+++ src/Components/Sign.jsx
@@ -1,27 +1,31 @@
 import react from "react"
+import { useState } from "react";
+
 
 const Sign = () => {
     return (
     <>
     <div className="flex items-center justify-center min-h-screen">
-    <div className="bg-black p-10 h-[500px] w-[500px] mr-[5px] rounded-[20px] shadow-lg">
+    <div className="bg-blue-400 p-10 h-[500px] w-[500px] mr-[5px] rounded-[20px] shadow-lg">
         <h1 className="text-white text-4xl font-bold">Hey there! </h1>
            <div className="mt-8">
             <p className="text-white text-4xl  ">Enter your personal details to start with us!</p>
             <div className="items-center justify-between">
-           
+            <button className=" bg-blue-700  hover:bg-blue-500  text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-[120px] w-[350px]" type="button">Sign up</button>
            </div>
-           <button className="bg-blue-700  hover:bg-blue-500  text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-[120px] w-[350px]" type="button">Sign up</button>
+           
 
            </div>
+          
       </div>
-      <div className="bg-black p-10 h-[500px] w-[500px] rounded-[20px] shadow-lg">
+    
+      <div className="bg-pink-300 p-10 h-[500px] w-[500px] rounded-[20px] shadow-lg">
         <h1 className="text-white text-4xl font-bold">Login. </h1>
            <div className="mt-8">
             <label className="block text-white text-sm font-bold mb-2" htmlFor = "Username">Username</label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+            <input className="shadow appearance-none border rounded-[13px] w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
             <label className="block mt-4 text-white text-sm font-bold mb-2" htmlFor = "Password">Password</label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" id ="password" type="password" placeholder="password"/>
+            <input className="shadow appearance-none border rounded-[13px] w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" id ="password" type="password" placeholder="password"/>
            </div>
            <div className="flex justify-end py-4">
            <p className="text-white justify-end">Forgot Password ?</p>
